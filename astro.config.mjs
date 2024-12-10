2acba1a21f1e2d3004c8ea9609a01cff598da9b6
frandepy2 <frandepy@fpuna.edu.py>
2024-12-10 01:35:20 -0300

    Fix astro config to get css config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
-  base: '/portfolio/',
-  integrations: [tailwind()]
+  output: "static",
+  base: "./",
+  integrations: [tailwind({ applyBaseStyles: true })],
 });
